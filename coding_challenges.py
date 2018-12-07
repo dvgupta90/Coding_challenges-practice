a2dc9e106c4ae2b9cc4d6aca94665fd197df1645
vagrant <vagrant@vagrant.vm>
2018-12-07 22:37:00 +0000

    two sum

--- coding_challenges.py
+++ coding_challenges.py
@@ -13,9 +13,25 @@ class Solution:
         :type target: int
         :rtype: List[int]
         """
-        for i,item in enumerate(nums):
-            for i2, item2 in enumerate(nums):
+        for i,num1 in enumerate(nums):
+            for i2, num2 in enumerate(nums):
                 if item + item2 == target:
                     return [i,i2]
+
+################# method 2 using dict ########################################
+
+    
+
+    def twoSum(self, nums, target):
+        
+        complement_num_dict = {}
+            
+        for i in range(len(nums)):
+            if nums[i] in complement_num_dict: #checking if num in given list is also in dict keys
+                return [complement_num_dict[nums[i]],i]
+            else:
+                complement_num_dict[target - nums[i]] = i
+
+
 
                     
